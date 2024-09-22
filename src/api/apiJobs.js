4b1b4f7701a35739241c796c31cf21445c2c6cae
vansh.shah <vanshshah174@gmail.com>
2024-09-22 15:30:16 -0400

    Job_listing&&Apijobs_Implemented

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -1,16 +1,28 @@
 import supabaseClient from "@/utils/supabase";
 
 
-export async function getJobs(token,{location,company_id,searchQuery}) {
+export async function getJobs(token, { location, company_id, searchQuery }) {
     const supabase = await supabaseClient(token);
 
+    let query = supabase.from("jobs").select("*,company:companies(name,logo_url),saved: saved_jobs(id)");
 
-    let query = supabase.from("jobs").select("*");
+    if (location) {
+        query = query.eq("location", location)
+    }
+
+    if (company_id) {
+        query = query.eq("company_id", company_id)
+    }
+
+    if (searchQuery) {
+        query = query.ilike("searchQuery", `%${searchQuery}%`);
+    }
+
 
     const { data, error } = await query;
 
-    if(error) {
-        console.error("Error fetching Jobs:" ,error);
+    if (error) {
+        console.error("Error fetching Jobs:", error);
         return null;
     }
 
--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,26 +1,62 @@
 import { getJobs } from "@/api/apiJobs";
 import useFetch from "@/hooks/use-fetch";
-import { useEffect } from "react";
-
+import { useUser } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
+import { BarLoader } from "react-spinners";
+import JobCard from "@/components/job-card";
 
 const JobListing = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+  const [company_id, setCompany_id] = useState("");
+  const { isLoaded } = useUser();
 
+  const {
+    fn: fnjobs,
+    data: jobs,
+    loading: loadingJobs,
+  } = useFetch(getJobs, {
+    location,
+    company_id,
+    searchQuery,
+  });
 
-    const {
-      fn:fnjobs,
-      data:dataJobs,
-      loading:loadingJobs,
-    } = useFetch(getJobs , {})
+  useEffect(() => {
+    if (isLoaded) fnjobs();
+  }, [isLoaded, location, company_id, searchQuery]);
 
-    console.log(dataJobs);
-    
-    useEffect(() => {
-      fnjobs
-    },[])
-
-    return <div>JobListing</div>
-
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
   }
-  
 
-export default JobListing
+  return (
+    <div>
+      <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
+        Latest Jobs
+      </h1>
+
+      {/* Add Filter */}
+
+      {loadingJobs && (
+        <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
+      )}
+
+      { loadingJobs === false && (
+        <div>
+          {jobs?.length ? (
+            jobs.map((job)=>{
+              return <JobCard key={job.id} job={job} />
+            })
+          ) : (
+            <div> No Jobs Found 😢 </div>
+          )
+
+          }
+          </div>
+      )}
+
+    </div>
+  );
+};
+
+export default JobListing;
