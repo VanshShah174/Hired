b50ab34122f8a73580bedd8f2bb17e601b946348
vansh.shah <vanshshah174@gmail.com>
2024-09-28 16:27:22 -0400

    search and filter jobs implemented

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -1,60 +1,62 @@
 import supabaseClient from "@/utils/supabase";
 
-
+// Fetch Jobs
 export async function getJobs(token, { location, company_id, searchQuery }) {
-    const supabase = await supabaseClient(token);
-    let query = supabase
-      .from("jobs")
-      .select("*, saved: saved_jobs(id), company: companies(name,logo_url)");
-  
-    if (location) {
-      query = query.eq("location", location);
-    }
-  
-    if (company_id) {
-      query = query.eq("company_id", company_id);
-    }
-  
-    if (searchQuery) {
-      query = query.ilike("title", `%${searchQuery}%`);
-    }
-  
-    const { data, error } = await query;
-  
-    if (error) {
-      console.error("Error fetching Jobs:", error);
-      return null;
-    }
-  
-    return data;
+  const supabase = await supabaseClient(token);
+  let query = supabase
+    .from("jobs")
+    .select("*, saved: saved_jobs(id), company: companies(name,logo_url)");
+
+  if (location) {
+    query = query.eq("location", location);
   }
 
+  if (company_id) {
+    query = query.eq("company_id", company_id);
+  }
+
+  if (searchQuery) {
+    query = query.ilike("title", `%${searchQuery}%`);
+  }
+
+  const { data, error } = await query;
+
+  if (error) {
+    console.error("Error fetching Jobs:", error);
+    return null;
+  }
+
+  return data;
+}
+
 export async function saveJob(token, { alreadySaved }, saveData) {
-    const supabase = await supabaseClient(token);
+  const supabase = await supabaseClient(token);
 
-    if (alreadySaved) {
-        const { data, error: deleteError } = await supabase
-            .from("saved_jobs")
-            .delete()
-            .eq("job_id", saveData.job_id)
+  if (alreadySaved) {
+    // If the job is already saved, remove it
+    const { data, error: deleteError } = await supabase
+      .from("saved_jobs")
+      .delete()
+      .eq("job_id", saveData.job_id);
 
-        if (deleteError) {
-            console.error("Error deleting Saved Job:", deleteError);
-            return null;
-        }
-        return data;
-    } else {
-        const { data, error: insertError } = await supabase
-            .from("saved_Jobs")
-            .insert([saveData])
-            .select()
-
-        if (insertError) {
-            console.error("Error fetching Jobs:", insertError);
-            return null;
-        }
-
-        return data;
+    if (deleteError) {
+      console.error("Error removing saved job:", deleteError);
+      return null;
     }
 
+    return data;
+  } else {
+    // If the job is not saved, add it to saved jobs
+    const { data, error: insertError } = await supabase
+      .from("saved_jobs")
+      .insert([saveData])
+      .select();
+
+    if (insertError) {
+      console.error("Error saving job:", insertError);
+      return data;
+    }
+
+    return data;
+  }
 }
--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -4,6 +4,18 @@ import { useUser } from "@clerk/clerk-react";
 import { useEffect, useState } from "react";
 import { BarLoader } from "react-spinners";
 import JobCard from "@/components/job-card";
+import { getCompanies } from "@/api/apiCompanies";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { State } from "country-state-city";
 
 const JobListing = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -21,9 +33,30 @@ const JobListing = () => {
     searchQuery,
   });
 
+  const { fn: fnCompanies, data: companies } = useFetch(getCompanies);
+
+  useEffect(() => {
+    if (isLoaded) fnCompanies;
+  }, [isLoaded]);
+
   useEffect(() => {
     if (isLoaded) fnJobs();
   }, [isLoaded, location, company_id, searchQuery]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    let formData = new FormData(e.target);
+
+    const query = formData.get("search-query");
+    if (query) setSearchQuery(query);
+  };
+
+  const clearFilters = () => {
+    setSearchQuery("");
+    setLocation("");
+    setLocation("")
+  }
+
 
   if (!isLoaded) {
     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
@@ -36,6 +69,60 @@ const JobListing = () => {
       </h1>
 
       {/* Add Filter */}
+
+      <form
+        onSubmit={handleSearch}
+        className="h-14 flex w-full gap-2 items-center mb-3"
+      >
+        <Input
+          type="text"
+          placeholder="Search Jobs By Title..."
+          name="search-query"
+          className="h-full flex-1 px-4 text-md"
+        />
+        <Button type="submit" className="h-full sm:w-28" variant="blue">
+          Search
+        </Button>
+      </form>
+
+      <div className="flex flex-col sm:flex-row gap-2">
+        <Select value={location} onValueChange={(value) => setLocation(value)}>
+          <SelectTrigger>
+            <SelectValue placeholder="Filter By Location" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {State.getStatesOfCountry("IN").map(({name})=>{
+                return (
+                  <SelectItem key={name} value={name}>{name}</SelectItem>
+                );                
+              })}
+
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Select value={company_id} onValueChange={(value) => setCompany_id(value)}>
+          <SelectTrigger>
+            <SelectValue placeholder="Filter By Company" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {companies?.map(({name, id})=>{
+                return (
+                  <SelectItem key={name} value={id}>{name}</SelectItem>
+                );                
+              })}
+
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Button onClick={clearFilters} className="sm:w-1/2" variant="destructive">
+              Clear Filters
+        </Button>
+
+      </div>
 
       {loadingJobs && (
         <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
